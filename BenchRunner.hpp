d8f8a02c353f43900378eca3f0f08009423430ca
David <david@templatious.org>
2015-03-24 21:09:27 +0200

    int file

---
+++ BenchRunner.hpp
@@ -0,0 +1,21 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  BenchRunner.hpp
+ *
+ *    Description:  Benchmark main interface
+ *
+ *        Version:  1.0
+ *        Created:  2015.03.24 21:09:01
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Deividas Kazlauskas (dk), ne-bobu@reikalas.lt
+ *        Company:  Company and shit
+ *
+ * =====================================================================================
+ */
+
+namespace IsItFast {
+
+}

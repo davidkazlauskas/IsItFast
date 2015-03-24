93391b1b76d3da421d3257ea8b02879fe3bf7e68
David <david@templatious.org>
2015-03-24 21:08:00 +0200

    boom

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  Get ready to roll
+ *
+ *        Version:  1.0
+ *        Created:  08/30/2014 09:19:10 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Deividas Kazlauskas (dk), ne-bobu@reikalas.lt
+ *        Company:  Company and shit
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+
+int main(int argc,char* argv[]) {
+}

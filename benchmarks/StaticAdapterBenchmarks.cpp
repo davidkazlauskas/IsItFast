a59aa6ed6b576755093170ff643152c8a62f44fb
David <david@templatious.org>
2015-03-26 17:54:00 +0200

    boom

---
+++ benchmarks/StaticAdapterBenchmarks.cpp
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  StaticAdapterBenchmarks.cpp
+ *
+ *    Description:  Benchmark for common operations
+ *
+ *        Version:  1.0
+ *        Created:  2015.03.26 17:50:22
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Deividas Kazlauskas (dk), ne-bobu@reikalas.lt
+ *        Company:  Company and shit
+ *
+ * =====================================================================================
+ */
+
+#include "../BenchRunner.hpp"
+
+namespace {
+
+    bool addBenchmarks() {
+        return true;
+    }
+
+    static bool didAdd = addBenchmarks();
+}
+

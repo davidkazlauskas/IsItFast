0d16df2ca4526146870a495d24418877e450b66d
David <david@templatious.org>
2015-03-30 19:56:46 +0300

    boiler

--- benchmarks/FunctionalManipulationBenchmarks.cpp
+++ benchmarks/FunctionalManipulationBenchmarks.cpp
@@ -18,6 +18,8 @@
 
 #include "../BenchRunner.hpp"
 
+TEMPLATIOUS_TRIPLET_STD;
+
 namespace {
     IsItFast::TimeResolution* tr = IsItFast::TimeResolutionFactory::s_curr;
 }
@@ -25,6 +27,28 @@ namespace {
 namespace IsItFast {
 
     bool ifSelectCpy() {
+        Benchmark add(tr,50,"filter-select","Traverse select some"
+            " integers.");
+
+        auto sptr = std::make_shared< std::vector<int> >();
+        SA::add(*sptr,SF::seqL(10000));
+
+        auto boiler = [sptr]() {
+            volatile double sum = 0;
+
+            int len = sptr->size();
+            for (int i = 0; i < len; ++i)
+            {
+                if (i % 7 == 0) {
+                    sum += sptr->at(i) * 7;
+                }
+            }
+        };
+
+        add.addTask("BOILERPLATE","Imperative version",
+            boiler);
+
+        BenchCollection::s_inst.addBenchmark(std::move(add));
         return true;
     }
 

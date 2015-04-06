021150f8066cefdeb2f9ab204d7afe565b762b62
David <david@templatious.org>
2015-04-06 10:03:16 +0300

    duh

--- drawgraph.js
+++ drawgraph.js
@@ -1,14 +1,20 @@
 (function() {
     var paintGraph = function(data) {
         var barchartData;
-        alert("inner");
-    }
+        barchartData = {
+            labels: data.benchmarks.map(function(i) {
+                alert(i.name);
+                return i.name;
+            })
+        };
+    };
 
     window.initPage = function() {
         return $.ajax('./results.json', {
             type: 'GET',
             dataType: 'json',
             success: function(data, textStatus, jqXHR) {
+                alert(data);
                 paintGraph(data);
             },
             failure: function(data, textStatus, jqXHR) {

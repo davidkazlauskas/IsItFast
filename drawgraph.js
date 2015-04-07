5f48ef7db0ce6b0a02ba1e2a1e44bc66de63a3e3
David <david@templatious.org>
2015-04-07 19:09:14 +0300

    finally there

--- drawgraph.js
+++ drawgraph.js
@@ -1,4 +1,7 @@
 (function() {
+    //var legendTemplate = '<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].fillColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>';
+    var legendTemplate = '<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].fillColor%>\"><%if(datasets[i].label){%><%}%><%=datasets[i].label%></span></li><%}%></ul>';
+
     var findData = function(data,name) {
         var i,len;
         len = data.benchmarks.length;
@@ -29,35 +32,42 @@
         theData = findData(data,"filter-select");
         barchartData = {
             labels: [
-                findTime(theData,"BOILERPLATE").full_name,
-                findTime(theData,"templatious_select").full_name
+                "Filter select benchmark (filter-select)"
             ],
             datasets: [
             {
+                label: "Boilerplate version",
                 fillColor: "rgba(100,100,100,0.7)",
+                strokeColor: "rgba(77,77,77,0.8)",
+                highlightFill: "rgba(150,150,150,0.8)",
+                highlightStroke: "rgba(128,128,128,0.8)",
+                data: [
+                    findTime(theData,"BOILERPLATE").time
+                ]
+            }
+            ,
+            {
+                label: "Templatious version",
+                fillColor: "rgba(0,200,0,0.7)",
                 strokeColor: "rgba(0,77,0,0.8)",
                 highlightFill: "rgba(0,255,0,0.8)",
                 highlightStroke: "rgba(0,128,0,0.8)",
                 data: [
-                    findTime(theData,"BOILERPLATE").time,
                     findTime(theData,"templatious_select").time
                 ]
             }
-            //,
-            //{
-                //fillColor: "rgba(0,200,0,0.7)",
-                //strokeColor: "rgba(0,77,0,0.8)",
-                //highlightFill: "rgba(0,255,0,0.8)",
-                //highlightStroke: "rgba(0,128,0,0.8)",
-                //data: []
-            //}
             ]
         };
         moreData = {
-            responsive: true
+            responsive: true,
+            barValueSpacing: 20,
+            barDatasetSpacing: 40,
+            legendTemplate: legendTemplate
         };
-        ctx = document.getElementById("bench-canvas").getContext("2d");
-        window.avgLoad = new Chart(ctx).Bar(barchartData,moreData);
+        ctx = document.getElementById("filter-select-canvas").getContext("2d");
+        var chart = new Chart(ctx).Bar(barchartData,moreData);
+        window.avgLoad = chart;
+        document.getElementById("filter-select-legend").innerHTML = chart.generateLegend();
     };
 
     window.initPage = function() {
